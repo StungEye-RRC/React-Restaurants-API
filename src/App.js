ae2906b8e1f324512012a7172d22e96b808ce7b8
Kyle Geske <stungeye@gmail.com>
2020-03-28 07:19:43 -0500

    Using material ui components now.

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
   const [restaurants, setRestaurants] = useState([]);
   const [newName, setNewName] = useState("");
   const [newDescription, setNewDescription] = useState("");
+  const [open, setOpen] = useState(false);
 
   /* Fetch all restaurants and save them into state. */
   /* Request does not include error handling logic. */
@@ -33,6 +34,16 @@ function App() {
       });
     });
   }, []);
+
+  /* Open/close helpers for our modal dialog. */
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   /* Add a new restaurant using the data supplied in the HTML inputs. */
   /* No front-end form/data validation is currently present.          */
@@ -51,32 +62,90 @@ function App() {
       .then(response => response.json())
       .then(newRestaurant => setRestaurants([...restaurants, newRestaurant]));
     /* The ... is the Javascript spread operator. */
+
+    handleClose();
   };
 
   return (
     <div className="App">
-      <div>
-        <input
-          onChange={e => setNewName(e.target.value)}
-          placeholder="Restaurant Name"
-        />
-        <input
-          onChange={e => setNewDescription(e.target.value)}
-          placeholder="Description"
-        />
-        <button onClick={addRestaurant}>Create New Restaurant</button>
-      </div>
+      <Container>
+        <Typography variant="h1" gutterBottom>
+          Restaurants
+        </Typography>
 
-      {restaurants.length === 0 && (
-        <p>No restaurants found. Check API server.</p>
-      )}
+        <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+          New Restaurant
+        </Button>
 
-      {restaurants.map(restaurant => (
-        <div key={restaurant.id}>
-          <h1>{restaurant.name}</h1>
-          <p>{restaurant.description}</p>
-        </div>
-      ))}
+        <Grid container spacing={5} style={{ marginTop: "2rem" }}>
+          {restaurants.map(restaurant => (
+            <Grid key={restaurant.id} item xs={12} sm={6} md={4} lg={4} xl={3}>
+              <Card>
+                <CardHeader
+                  avatar={
+                    <Avatar style={{ backgroundColor: "#EE3333" }}>
+                      {restaurant.name.match(/\b(\w)/g).join("")}
+                    </Avatar>
+                  }
+                  titleTypographyProps={{ variant: "h6" }}
+                  title={restaurant.name}
+                />
+                <CardContent>
+                  <Typography
+                    paragraph
+                    color="textSecondary"
+                    style={{ height: "10rem" }}
+                  >
+                    {truncateString(restaurant.description, 220)}
+                  </Typography>
+                  {/* <Typography paragraph color="textSecondary">
+                    Number of Dishes: {restaurant.dishes.length}
+                  </Typography> */}
+                </CardContent>
+                <CardActions style={{ justifyContent: "right" }}>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Add Restaurant</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Add a new website by providing a name and a description.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Restaurant Name"
+              fullWidth
+              onChange={e => setNewName(e.target.value)}
+            />
+            <TextField
+              margin="dense"
+              label="Description"
+              fullWidth
+              onChange={e => setNewDescription(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={addRestaurant} color="primary">
+              Add
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Container>
     </div>
   );
 }
